4585185abe674d4a4111610599ab3997701906fc
Chintak <chintak@promactinfo.com>
2023-05-08 22:02:50 +0530

    Adds support for Android 13

--- multiimagepicker/src/main/java/com/creativechintak/multiimagepicker/StartImagePickerActivity.kt
+++ multiimagepicker/src/main/java/com/creativechintak/multiimagepicker/StartImagePickerActivity.kt
@@ -4,8 +4,8 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ckdroid.dynamicpermissions.PermissionStatus
 import com.ckdroid.dynamicpermissions.PermissionUtils
 
@@ -14,14 +14,18 @@ class StartImagePickerActivity : AppCompatActivity() {
     private var maxSelectionLimit = 0
     private var showAlbums = false
 
-    companion object{
+    companion object {
         private const val MAX_PICTURE_LIMIT = 30
 
         private val PERMISSIONS_GALLERY_WITH_WRITE: Array<String>
             get() {
                 val permissions = mutableListOf<String>()
 
-                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    permissions.add(Manifest.permission.READ_MEDIA_IMAGES)
+                } else {
+                    permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
 
                 return permissions.toTypedArray()
             }
@@ -38,29 +42,35 @@ class StartImagePickerActivity : AppCompatActivity() {
     }
 
     private fun initParams() {
-        maxSelectionLimit = intent.getIntExtra(Constants.BUNDLE_MAX_SELECTION_LIMIT,
-            MAX_PICTURE_LIMIT)
+        maxSelectionLimit = intent.getIntExtra(
+            Constants.BUNDLE_MAX_SELECTION_LIMIT,
+            MAX_PICTURE_LIMIT
+        )
 
-        if(maxSelectionLimit <= 0 || maxSelectionLimit > MAX_PICTURE_LIMIT){
+        if (maxSelectionLimit <= 0 || maxSelectionLimit > MAX_PICTURE_LIMIT) {
             maxSelectionLimit = MAX_PICTURE_LIMIT
         }
 
-        showAlbums = intent.getBooleanExtra(Constants.BUNDLE_SHOW_ALBUMS,false)
+        showAlbums = intent.getBooleanExtra(Constants.BUNDLE_SHOW_ALBUMS, false)
     }
 
     private fun checkForGalleryPermission(hasAskedOnce: Boolean = false) {
-        val result = PermissionUtils.checkAndRequestPermissions(this, PERMISSIONS_GALLERY_WITH_WRITE.toMutableList(),
-            REQUEST_PICK_IMAGE_PERMISSION,hasAskedOnce)
+        val result = PermissionUtils.checkAndRequestPermissions(
+            this, PERMISSIONS_GALLERY_WITH_WRITE.toMutableList(),
+            REQUEST_PICK_IMAGE_PERMISSION, hasAskedOnce
+        )
 
-        when(result.finalStatus){
+        when (result.finalStatus) {
             PermissionStatus.ALLOWED -> {
                 openImagePickerActivity()
             }
+
             PermissionStatus.DENIED_PERMANENTLY -> {
-                if(hasAskedOnce){
+                if (hasAskedOnce) {
 
                 }
             }
+
             else -> Unit
         }
     }
@@ -72,7 +82,7 @@ class StartImagePickerActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == REQUEST_PICK_IMAGE_PERMISSION) {
+        if (requestCode == REQUEST_PICK_IMAGE_PERMISSION) {
             checkForGalleryPermission(true)
         }
     }
@@ -87,8 +97,8 @@ class StartImagePickerActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == REQUEST_PICK_IMAGE){
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == REQUEST_PICK_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
                 setResult(resultCode, data)
             }
             onBackPressed()
